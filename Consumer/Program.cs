2c912fcc147a1ac282d08a1cf6d3796919c6025f
arrafeeahsan <arrafeeahsan@gmail.com>
2023-01-08 15:54:48 +0600

    competing consumers

---
+++ Consumer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "letterbox",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+);
+
+
+
+channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+var consumer = new EventingBasicConsumer(channel);
+var random = new Random(); // competing consumer
+
+consumer.Received += (Model, ea) =>
+{
+    var processingTime = random.Next(1, 6); // competing consumer
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Received: {message} will take {processingTime} to process");
+
+    Task.Delay(TimeSpan.FromSeconds(processingTime)).Wait();
+
+    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+};
+
+channel.BasicConsume(queue: "letterbox", autoAck: true, consumer: consumer);
+Console.WriteLine("Consuming");
+Console.ReadKey();
---
+++ Producer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "letterbox",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+);
+
+var random = new Random();
+
+var messageId = 1;
+
+while (messageId == 10)
+{
+
+    var publishingTime = random.Next(1, 4);
+
+    var message = $"Sending MessageId: {messageId}";
+    var encodedMessage = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish("", "letterbox", null, encodedMessage);
+    Console.WriteLine($"Publshed Message: {message}");
+
+    Task.Delay(TimeSpan.FromSeconds(publishingTime)).Wait();
+
+    messageId++;
+}
+
+
+
